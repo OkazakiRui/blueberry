f8d8b4413586a99fffd3d1be2d360d7d1368cd58
RuiOkazaki <okazaki200226@gmail.com>
2022-04-23 16:28:11 +0900

    feat: extends

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,83 @@
-type Animal = {
-  age: number;
-};
-type Human = {
-  age: number;
-  name: string;
-};
+{
+  type Animal = {
+    age: number;
+  };
+  type Human = {
+    age: number;
+    name: string;
+  };
 
-type AnimalFamily = {
-  familyName: string;
-  mother: Animal;
-  father: Animal;
-  child: Animal;
-};
-type HumanFamily = {
-  familyName: string;
-  mother: Human;
-  father: Human;
-  child: Human;
-};
+  type AnimalFamily = {
+    familyName: string;
+    mother: Animal;
+    father: Animal;
+    child: Animal;
+  };
+  type HumanFamily = {
+    familyName: string;
+    mother: Human;
+    father: Human;
+    child: Human;
+  };
+}
+{
+  type User<T> = {
+    name: string;
+    child: T;
+  };
+  type Family<Parent, Child> = {
+    mother: Parent;
+    father: Parent;
+    child: Child;
+  };
+  const obj: Family<number, string> = {
+    mother: 12,
+    father: 123,
+    child: 'hoge',
+  };
+}
+{
+  type HasName = {
+    name: string;
+  };
+  type Family<Parent extends HasName, Child extends HasName> = {
+    mother: Parent;
+    father: Parent;
+    child: Child;
+  };
+  // const obj: Family<number, string> = {
+  //   mother: 12,
+  //   father: 123,
+  //   child: 'hoge',
+  // };
+}
+{
+  type HasName = {
+    name: string;
+  };
+  type Family<Parent extends HasName, Child extends HasName> = {
+    mother: Parent;
+    father: Parent;
+    child: Child;
+  };
+  type Animal = {
+    name: string;
+  };
+  type Human = {
+    name: string;
+    age: number;
+  };
+  type T = Family<Animal, Human>;
+  const obj: T = {
+    mother: {
+      name: 'mother',
+    },
+    father: {
+      name: 'father',
+    },
+    child: {
+      name: 'child',
+      age: 0,
+    },
+  };
+}
